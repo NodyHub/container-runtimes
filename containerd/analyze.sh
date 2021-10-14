7b137e4b298eb5f490e4895a34f4c6cc7c5fe6f0
NodyHub <github@janharrie.de>
2021-10-14 11:11:29 +0200

    Updated script

--- containerd/analyze.sh
+++ containerd/analyze.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-tcpdump -w traffic.pcap port 53 &
+tcpdump -w traffic.pcap port 53 2> /dev/null &
 TCPDUMP_PID=$!
-
-sleep 5
 
 DOCKER_IMAGE=docker.io/library/alpine:latest
 DOCKER_IMAGE=quay.io/jitesoft/alpine:latest
 
+ctr image rm $DOCKER_IMAGE 2> /dev/null
 ctr image pull $DOCKER_IMAGE
 
 kill $TCPDUMP_PID
+
+echo
+echo
+echo DNS Querys:
+echo -----------
+tcpdump -r traffic.pcap port 53 2> /dev/null | grep 'A?' | cut -d " " -f 9 | uniq
+echo
 
 exit 0
 
